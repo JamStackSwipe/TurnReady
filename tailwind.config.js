6ff2322dcc1c533e3fd3b1e7f590212a103b5e3e
JamStackSwipe <eric@hochacool.com>
2025-06-12 23:51:06 -0500

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Scan your app for Tailwind classes
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
