40b03cd0bd7ee6a6b1740683596e857ba14667bc
JamStackSwipe <eric@hochacool.com>
2025-06-15 22:25:02 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,32 +3,41 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
+
+// Shared Components
 import Navbar from './components/Navbar';
 import RequireRole from './components/RequireRole';
 
+// Public Pages
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Profile from './pages/Profile';
+import TechSignup from './pages/TechSignup';
+import ClientSignup from './pages/ClientSignup';
 
-import ClientDashboard from './pages/ClientDashboard';
-import MyProperties from './pages/MyProperties';
-import MyRequests from './pages/MyRequests';
-
-import TechDashboard from './pages/TechDashboard';
-import MyJobs from './pages/MyJobs';
-import TechProfileSetup from './pages/TechProfileSetup';
-
+// Job Pages
 import SubmitJob from './pages/SubmitJob';
 import JobBoard from './pages/JobBoard';
 import JobDetails from './pages/JobDetails';
 import JobUpdate from './pages/JobUpdate';
 import PartsRequest from './pages/PartsRequest';
 
+// Client Pages (role: client + admin)
+import ClientDashboard from './pages/ClientDashboard';
+import MyProperties from './pages/MyProperties';
+import MyRequests from './pages/MyRequests';
+
+// Tech Pages (role: tech + admin)
+import TechDashboard from './pages/TechDashboard';
+import MyJobs from './pages/MyJobs';
+import TechProfileSetup from './pages/TechProfileSetup';
+
+// Admin Pages (role: admin)
 import AdminDashboard from './pages/AdminDashboard';
 import AdminJobs from './pages/AdminJobs';
 import AdminReviews from './pages/AdminReviews';
 import AdminUsers from './pages/AdminUsers';
-import AdminPartsRequests from './pages/AdminPartsRequests'; // ✅ New
+import AdminPartsRequests from './pages/AdminPartsRequests';
 
 function App() {
   return (
@@ -41,8 +50,14 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/profile" element={<Profile />} />
+        <Route path="/signup/tech" element={<TechSignup />} />
+        <Route path="/signup/client" element={<ClientSignup />} />
         <Route path="/submit-job" element={<SubmitJob />} />
         <Route path="/job-board" element={<JobBoard />} />
+        <Route path="/job/:id" element={<JobDetails />} />
+        <Route path="/job-update/:jobId" element={<JobUpdate />} />
+        <Route path="/parts-request/:jobId" element={<PartsRequest />} />
+        <Route path="/parts-request" element={<PartsRequest />} />
 
         {/* Client Views */}
         <Route path="/client-dashboard" element={
@@ -77,10 +92,6 @@ function App() {
             <TechProfileSetup />
           </RequireRole>
         } />
-        <Route path="/job/:id" element={<JobDetails />} />
-        <Route path="/job-update/:jobId" element={<JobUpdate />} />
-        <Route path="/parts-request/:jobId" element={<PartsRequest />} />
-        <Route path="/parts-request" element={<PartsRequest />} />
 
         {/* Admin Views */}
         <Route path="/admin" element={
